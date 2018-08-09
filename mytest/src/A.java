29bcd381f4a62ff0416565e3ac8dc7d19a365a41
taojun <taojun@gistandard.cn>
2018-08-09 15:54:06 +0800

    提交文件，创建文件至dev

--- mytest/src/A.java
+++ mytest/src/A.java
@@ -1,2 +1,14 @@
 public class A {
+    public static void main(String[] args) {
+        int a1 = 0;
+        int a2 = 1;
+        int a3 = 2;
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+        System.out.println("A------------------------" + (a1 + a2 - a3));
+    }
 }
--- mytest/src/C.java
+++ mytest/src/C.java
@@ -1,2 +1,13 @@
 public class C {
+    public static void main(String[] args) {
+        int c1 = 0;
+        int c2 = 1;
+        int c3 = 2;
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+        System.out.println("C------------------------" + (c1 + c2 - c3));
+    }
 }
