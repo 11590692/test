8c8702ed17af2a0a3a8a079c579239cf560c701c
taojun <taojun@gistandard.cn>
2018-08-09 15:54:06 +0800

    提交文件，创建文件至dev

--- mytest/src/B.java
+++ mytest/src/B.java
@@ -1,2 +1,13 @@
 public class B {
+    public static void main(String[] args) {
+        int b1 = 0;
+        int b2 = 1;
+        int b3 = 2;
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+        System.out.println("B------------------------" + (b1 + b2 - b3));
+    }
 }
--- mytest/src/D.java
+++ mytest/src/D.java
@@ -1,2 +1,13 @@
 public class D {
+    public static void main(String[] args) {
+        int d1 = 0;
+        int d2 = 1;
+        int d3 = 2;
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+        System.out.println("D------------------------" + (d1 + d2 - d3));
+    }
 }
