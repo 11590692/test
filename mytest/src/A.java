f7a4fef4a09d7bd1e1cffcc391d37eb3dd4a86a2
taojun <taojun@gistandard.cn>
2018-08-10 10:03:31 +0800

    prod提交

--- mytest/src/A.java
+++ mytest/src/A.java
@@ -1,2 +1,9 @@
 public class A {
+    public static void main(String[] args){
+        System.out.println("dsa543534dqwd");
+        System.out.println("654654");
+        System.out.println("435");
+        System.out.println("43543523");
+        System.out.println("43543543");
+    }
 }
--- mytest/src/D.java
+++ mytest/src/D.java
@@ -1,2 +1,9 @@
 public class D {
+    public static void main(String[] args){
+        System.out.println("gre;
+        System.out.println("ger");
+        System.out.println("dsafredqwd");
+        System.out.println("wqd");
+        System.out.println("fv");
+    }
 }
